992cf06ee538bb56e8cb0d6cd4ece86b807c23c8
Yufei Lin <yufei_lin_ulyssess@outlook.com>
2020-10-15 22:29:54 -0400

    Amended files

--- server/app.py
+++ server/app.py
@@ -1,39 +1,33 @@
 import json
 from flask import Flask, jsonify
 from Database.checkTables import checkTableExists
-from Database.createTables import createTestTable
-from Database.fetchResult import fetchAllFromGDP
-from Database.insertData import insertGDPData
+from Database.testDB import createTestTable, fetchAllFromGDP, insertGDPData
 import pymysql
 app = Flask(__name__)
 
 db = pymysql.connect(
-        host = "localhost",
-        user = "root",
-        passwd = "123456", 
-        db = "test",
-       port = 3306
-    )
+    host="localhost",
+    user="root",
+    passwd="123456",
+    db="test",
+    port=3306
+)
 
-# print(checkTableExists(db, "gdp"))
-createTestTable(db)
-insertGDPData(db)
-fetchAllFromGDP(db)
-db.close()
 
-# @app.route('/gdp', methods=["GET"])
-# def index():
-#     cursor = db.cursor()
-#     cursor.execute("SELECT VERSION()")
-#     data = cursor.fetchone()
-#     print ("Database version : %s " % data)
-#     db.close()
-#     return json.dumps([
+@app.route('/gdp', methods=["GET"])
+def index():
+    result = fetchAllFromGDP(db)
+    db.close()
+    print(result)
+    return result
+
+
+app.run()
+
+# print( json.dumps([
 #         {"country": "USA", "value": 20.5},
 #         {"country": "China", "value": 13.4},
 #         {"country": "Germany", "value": 4.0},
 #         {"country": "Japan", "value": 4.9},
 #         {"country": "France", "value": 2.8}
-#     ])
-
-# app.run()
+#     ]))
