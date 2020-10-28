d45add26584ab51bf90288d8e3186dcb57669a50
Lin <yufei_lin_ulyssess@outlook.com>
2020-10-29 01:27:22 +0800

    Dash update

---
+++ dashboard/src/views/CriminalAnalysis.vue
@@ -0,0 +1,11 @@
+<template>
+  <a href="https://www.geotab.com/heat-in-the-city/" target="_blank"
+    ><img
+      src="https://gogwebp01.geotab.com/images/heat-in-the-city/embed.png"
+      alt="Heat in the City"
+      border="0" /></a
+  ><br /><span style="font-size: 12px">
+      Click image to open interactive version (via
+    <a href="https://www.geotab.com/">Geotab</a>).</span
+  >
+</template>
